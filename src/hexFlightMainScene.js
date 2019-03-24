dbc1e90d12b7b84a360a44bca129cad000776358
Rune Holmgren <Raane.Holm@gmail.com>
2019-03-24 23:27:53 +0100

    Adding camera movements.

--- src/hexFlightMainScene.js
+++ src/hexFlightMainScene.js
@@ -3,7 +3,7 @@
 
     spawnPlanes() {
       this.planes = [];
-      this.planeCount = 24;
+      this.planeCount = 16;
       this.planeDistance = 15;
       this.planeShaders = [ new THREE.ShaderMaterial(SHADERS.HexSolid),
                             new THREE.ShaderMaterial(SHADERS.HexFlight),
@@ -66,6 +66,11 @@
       var LONGDIST = 50;
 
       this.camera.position.z = -(frame - 6030) / 5;
+      var camerapandist = 10;
+      var camerapanangler = 1.4;
+      this.camera.position.y = -camerapandist * (0.5 * Math.sin(frame/50) + 0.5);
+
+      this.camera.rotation.x = -this.camera.position.y/ camerapandist / camerapanangler;
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
       for (var i = 0; i < this.planeCount; i++) {
         this.planes[i].material.uniforms.frame.value = 0.2 * frame + i * 100;
