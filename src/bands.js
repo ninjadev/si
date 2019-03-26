51610ec85bf2cc733477afdbf6ad20528f13630a
Stian Jensen <me@stianj.com>
2019-03-26 22:44:48 +0100

    Add animations and tweak bands positions

--- src/bands.js
+++ src/bands.js
@@ -11,13 +11,32 @@
       this.bands = [];
       this.drapes = [];
 
+      this.positions = [
+        {rotation: 0, x: -90, y: 0},
+        {rotation: -.5, x: -80, y: 0},
+        {rotation: 0, x: 0, y: 0},
+        {rotation: 0.3, x: 30, y: 30},
+        {rotation: Math.PI / 2, x: 20, y: 20},
+        {rotation: -0.1, x: 50, y: 50},
+        {rotation: 0.2, x: -40, y: 0},
+        {rotation: -Math.PI / 2, x: -50, y: -50},
+        {rotation: -.65, x: -120, y: 0},
+        {rotation: 0.4, x: -90, y: -90},
+        {rotation: -0.7, x: -10, y: -100},
+        {rotation: 2, x: 150, y: 150},
+        {rotation: .4, x: 50, y: 50},
+        {rotation: 4, x: 50, y: 50},
+        {rotation: -1.7, x: -50, y: -60},
+        {rotation: .1, x: -100, y: 0},
+        {rotation: -.5, x: -50, y: 100},
+      ];
+
       const localRandom = window.Random(100);
 
-      for (let i=0; i < 20; i++) {
+      for (let i=0; i < this.positions.length; i++) {
         const container = new THREE.Object3D();
         const band = [];
-        const rotation = localRandom() * Math.PI;
-        const x = -150 + localRandom() * 300;
+        const {rotation, x, y} = this.positions[i];
         let width = 0;
         const n = 5 + (localRandom() * 5) | 0;
         const offsets = [];
@@ -47,9 +66,9 @@
         container.add(drape);
 
         container.rotation.z = rotation;
-        container.position.z = i * 5;
+        container.position.z = i * 7;
         container.position.x = x;
-        container.position.y = x;
+        container.position.y = y;
         this.scene.add(container);
       }
 
@@ -65,16 +84,23 @@
     update(frame) {
       super.update(frame);
 
+      const startFrame = 6582;
+
       for (let i = 0; i < this.bands.length; i++) {
         for (const line of this.bands[i]) {
           const path = line.path;
           path.material.uniforms.drawStart.value = 0;
-          path.material.uniforms.drawEnd.value =  2 * Math.sin(frame / 100 - i * 0.01) + 0.5 + 0.5 * Math.sin(i);
+          path.material.uniforms.drawEnd.value = lerp(0, 1, (frame - startFrame - i * 5) / 300);
           path.material.uniforms.wobbliness.value = 1;
         }
         const drape = this.drapes[i];
-        drape.position.y = clamp(-50, 0, -600 + 1200 * Math.sin(frame / 100 - i * 0.01) + 300 + 300 * Math.sin(i));
+        //drape.position.y = clamp(-50, 0, -600 + 1200 * Math.sin(frame / 100 - i * 0.01) + 300 + 300 * Math.sin(i));
+        drape.position.y = lerp(-600, 0, (frame - startFrame - i * 5) / 300);
       }
+
+      this.camera.position.z = easeIn(350, 150, (frame - startFrame) / 600);
+      this.camera.position.y = lerp(0, 40, (frame - startFrame) / 600);
+      this.camera.rotation.z = easeIn(0, -.2, (frame - startFrame) / 650);
     }
   }
 
