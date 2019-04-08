8eac2dd0df19e0e9c16788c64832ca057c79b496
iver56 <iver56@hotmail.com>
2019-04-08 22:10:45 +0200

    Show emojies consecutively. Will do infinizoom later.

--- src/emoji.js
+++ src/emoji.js
@@ -8,45 +8,75 @@
         }
       });
 
-      this.scene.background = new THREE.Color(0xffffff);
-
-      this.camera.position.z = 2000;
-      this.camera.position.x = 700;
-      this.camera.position.y = 1200;
+      this.camera.position.z = 4000;
+      this.camera.position.x = 1400;
+      this.camera.position.y = 400;
 
       this.emojiTextures = [];
       this.emojiMaterials = [];
       this.emojiGeometry = new THREE.PlaneGeometry(128, 128, 1);
-      this.tiles = [];
-      for (let emojiId in window.sunglassesMosaic.emojies) {
-        if (window.sunglassesMosaic.emojies.hasOwnProperty(emojiId)) {
-          this.emojiTextures[emojiId] = Loader.loadTexture(`res/emoji/${window.sunglassesMosaic.emojies[emojiId]}`);
+      this.wrappers = {};
+      this.tiles = {};
 
-          this.emojiMaterials[emojiId] = new THREE.MeshBasicMaterial({
-            map: this.emojiTextures[emojiId],
-            transparent: true
-          })
+      for (let mosaicKey in window.emojiMosaics) {
+        if (window.emojiMosaics.hasOwnProperty(mosaicKey)) {
+          let mosaic = window.emojiMosaics[mosaicKey];
+          for (let emojiId in mosaic.emojies) {
+            if (mosaic.emojies.hasOwnProperty(emojiId) && !this.emojiTextures.hasOwnProperty(emojiId)) {
+              console.log(mosaic.emojies[emojiId]);
+              this.emojiTextures[emojiId] = Loader.loadTexture(`res/emoji/${mosaic.emojies[emojiId]}`);
+              this.emojiMaterials[emojiId] = new THREE.MeshBasicMaterial({
+                map: this.emojiTextures[emojiId],
+                transparent: true
+              })
+            }
+          }
+
+          const wrapper = new THREE.Object3D();
+          this.tiles[mosaicKey] = [];
+
+          for (let i = 0; i < mosaic.tiles.length; i++) {
+            const tile = mosaic.tiles[i];
+            const material = this.emojiMaterials[tile.emoji_id];
+            const tileMesh = new THREE.Mesh(this.emojiGeometry, material);
+            tileMesh.position.x = tile.x;
+            tileMesh.position.y = 1800 - tile.y;
+            tileMesh.position.z = 0.01 * i;
+            this.tiles[mosaicKey].push(tileMesh);
+            wrapper.add(tileMesh);
+          }
+
+          this.wrappers[mosaicKey] = wrapper;
+          this.scene.add(wrapper);
         }
-      }
-
-
-      for (let i = 0; i < window.sunglassesMosaic.tiles.length; i++) {
-        const tile = window.sunglassesMosaic.tiles[i];
-        const material = this.emojiMaterials[tile.emoji_id];
-        const tileMesh = new THREE.Mesh(this.emojiGeometry, material);
-        tileMesh.position.x = tile.x;
-        tileMesh.position.y = 1800 - tile.y;
-        tileMesh.position.z = 0.01 * i;
-        this.tiles.push(tileMesh);
-        this.scene.add(tileMesh);
       }
     }
 
     update(frame) {
       super.update(frame);
+      const beanOffset = 1392;
+      const timing = {
+        goodMood: {start: 1392, end: 1392 + 12},
+        shades: {start: 1392 + 12, end: 1392 + 12 + 12},
+        pixelArt: {start: 1392 + 12 * 2, end: 1392 + 12 * 2 + 12},
+        dancingSkills: {start: 1392 + 12 * 3, end: 1392 + 12 * 3 + 12},
+        campingEquipment: {start: 1392 + 12 * 4, end: 1392 + 12 * 4 + 12},
+        hardware: {start: 1392 + 12 * 5, end: 1392 + 12 * 5 + 12},
+      };
 
-      for (let tile of this.tiles) {
-        tile.rotation.z = frame / 60;
+      for (let mosaicKey in window.emojiMosaics) {
+        if (window.emojiMosaics.hasOwnProperty(mosaicKey)) {
+          let wrapper = this.wrappers[mosaicKey];
+          let isActive = BEAN >= timing[mosaicKey].start && BEAN < timing[mosaicKey].end;
+          if (isActive) {
+            this.scene.add(wrapper);
+          } else {
+            this.scene.remove(wrapper);
+          }
+          for (let tile of this.tiles[mosaicKey]) {
+            tile.rotation.z = frame / 120;
+          }
+        }
       }
     }
   }
