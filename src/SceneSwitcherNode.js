f6d1411f91631733f7d534cc51c629f8d18830ee
Stian Jensen <me@stianj.com>
2019-04-09 21:39:46 +0200

    Sort the demo

--- src/SceneSwitcherNode.js
+++ src/SceneSwitcherNode.js
@@ -1,31 +1,25 @@
 (function(global) {
+
   class SceneSwitcherNode extends NIN.Node {
     constructor(id) {
       super(id, {
         inputs: {
           Startlogo: new NIN.TextureInput(),
-          A: new NIN.TextureInput(),
+          A1: new NIN.TextureInput(),
           A2: new NIN.TextureInput(),
-          B: new NIN.TextureInput(),
+          B1: new NIN.TextureInput(),
           B2: new NIN.TextureInput(),
-          C: new NIN.TextureInput(),
-          C2: new NIN.TextureInput(),
-          D: new NIN.TextureInput(),
-          E: new NIN.TextureInput(),
-          F: new NIN.TextureInput(),
-          Gintro: new NIN.TextureInput(),
-          G: new NIN.TextureInput(),
-          H: new NIN.TextureInput(),
-          I: new NIN.TextureInput(),
-          J: new NIN.TextureInput(),
-          K: new NIN.TextureInput(),
-          L: new NIN.TextureInput(),
-          L2: new NIN.TextureInput(),
-          M: new NIN.TextureInput(),
-          N: new NIN.TextureInput(),
-          O: new NIN.TextureInput(),
-          P: new NIN.TextureInput(),
+          A3: new NIN.TextureInput(),
+          A4: new NIN.TextureInput(),
+          B3: new NIN.TextureInput(),
+          B4: new NIN.TextureInput(),
+          Scratches: new NIN.TextureInput(),
+          B5intro: new NIN.TextureInput(),
+          B5: new NIN.TextureInput(),
+          B6: new NIN.TextureInput(),
           A5: new NIN.TextureInput(),
+          A6: new NIN.TextureInput(),
+          Outro: new NIN.TextureInput(),
         },
         outputs: {
           render: new NIN.TextureOutput(),
@@ -35,78 +29,55 @@
 
     beforeUpdate() {
       this.inputs.Startlogo.enabled = false;
-      this.inputs.A.enabled = false;
+      this.inputs.A1.enabled = false;
       this.inputs.A2.enabled = false;
-      this.inputs.B.enabled = false;
+      this.inputs.B1.enabled = false;
       this.inputs.B2.enabled = false;
-      this.inputs.C.enabled = false;
-      this.inputs.C2.enabled = false;
-      this.inputs.D.enabled = false;
-      this.inputs.E.enabled = false;
-      this.inputs.F.enabled = false;
-      this.inputs.Gintro.enabled = false;
-      this.inputs.G.enabled = false;
-      this.inputs.H.enabled = false;
-      this.inputs.I.enabled = false;
-      this.inputs.J.enabled = false;
-      this.inputs.K.enabled = false;
-      this.inputs.L.enabled = false;
-      this.inputs.L2.enabled = false;
-      this.inputs.M.enabled = false;
-      this.inputs.N.enabled = false;
-      this.inputs.O.enabled = false;
-      this.inputs.P.enabled = false;
+      this.inputs.A3.enabled = false;
+      this.inputs.A4.enabled = false;
+      this.inputs.B3.enabled = false;
+      this.inputs.B4.enabled = false;
+      this.inputs.Scratches.enabled = false;
+      this.inputs.B5intro.enabled = false;
+      this.inputs.B5.enabled = false;
+      this.inputs.B6.enabled = false;
       this.inputs.A5.enabled = false;
+      this.inputs.A6.enabled = false;
+      this.inputs.Outro.enabled = false;
 
       let selectedScene;
       if (BEAN < 24 * 5) {
         selectedScene = this.inputs.Startlogo;
-      } else if (BEAN < 48 * 3.5) {
-        selectedScene = this.inputs.A;
-      } else if (BEAN < 48 * 5.5) {
+      } else if (BEAN < 24 * 9) {
+        selectedScene = this.inputs.A1;
+      } else if (BEAN < 24 * 13) {
         selectedScene = this.inputs.A2;
-      } else if (BEAN < 48 * 8) {
-        selectedScene = this.inputs.B;
-      } else if (BEAN < 48 * 9.5) {
-        // Note, this scene intentionally swtiches early since it
-        // is piped into the next scene for a transition.
+      } else if (BEAN < 24 * 17) {
+        selectedScene = this.inputs.B1;
+      } else if (BEAN < 24 * 21) {
         selectedScene = this.inputs.B2;
-      } else if(BEAN < 48 * 12) {
-        selectedScene = this.inputs.C;
-      } else if(BEAN < 48 * 13) {
-        selectedScene = this.inputs.C2;
-      } else if(BEAN < 48 * 13.5) {
-        selectedScene = this.inputs.D;
-      } else if (BEAN < 48 * 16) {
-        selectedScene = this.inputs.E;
-      } else if (BEAN < 48 * 17 + 24) {
-        selectedScene = this.inputs.F;
-      } else  if (BEAN < 48 * 19) {
-        selectedScene = this.inputs.Gintro;
-      } else  if (BEAN < 48 * 20) {
-        selectedScene = this.inputs.G;
-      } else  if (BEAN < 48 * 21) {
-        selectedScene = this.inputs.H;
-      } else  if (BEAN < 48 * 21 + 24) {
-        selectedScene = this.inputs.I;
-      } else  if (BEAN < 48 * 24) {
-        selectedScene = this.inputs.J;
-      } else  if (BEAN < 48 * 26 + 12) {
-        selectedScene = this.inputs.K;
-      } else  if (BEAN < 48 * 27) {
-        selectedScene = this.inputs.L;
-      } else  if (BEAN < 48 * 28) {
-        selectedScene = this.inputs.L2;
-      } else  if (BEAN < 48 * 29) {
-        selectedScene = this.inputs.M;
-      } else  if (BEAN < 48 * 30) {
-        selectedScene = this.inputs.N;
-      } else  if (BEAN < 48 * 31) {
-        selectedScene = this.inputs.O;
-      } else  if (BEAN < 48 * 32) {
-        selectedScene = this.inputs.P;
-      } else {
+      } else if (BEAN < 24 * 25) {
+        selectedScene = this.inputs.A3;
+      } else if (BEAN < 24 * 29) {
+        selectedScene = this.inputs.A4;
+      } else if (BEAN < 24 * 33) {
+        selectedScene = this.inputs.B3;
+      } else if (BEAN < 24 * 37) {
+        selectedScene = this.inputs.B4;
+      } else if (BEAN < 24 * 45) {
+        selectedScene = this.inputs.Scratches;
+      } else if (BEAN < 24 * 47) {
+        selectedScene = this.inputs.B5intro;
+      } else if (BEAN < 24 * 49) {
+        selectedScene = this.inputs.B5;
+      } else if (BEAN < 24 * 53) {
+        selectedScene = this.inputs.B6;
+      } else if (BEAN < 24 * 57) {
         selectedScene = this.inputs.A5;
+      } else if (BEAN < 24 * 61) {
+        selectedScene = this.inputs.A6;
+      } else {
+        selectedScene = this.inputs.Outro;
       }
       selectedScene.enabled = true;
       this.selectedScene = selectedScene;
