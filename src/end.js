c2bf55b4e60834f3f63ba498bc23f1eca8b503cf
Stian Jensen <me@stianj.com>
2019-04-21 00:06:34 +0200

    Fix FSS end

--- src/end.js
+++ src/end.js
@@ -8,15 +8,6 @@
           render: new NIN.TextureOutput()
         }
       });
-      let path;
-      let line;
-
-      this.cameraX = 0;
-      this.cameraY = 0;
-      this.cameraDX = 0;
-      this.cameraDY = 0;
-      this.cameraDDX = 0;
-      this.cameraDDY = 0;
 
       this.flat = XWrite('FLAT');
       this.scene.add(this.flat);
@@ -47,43 +38,17 @@
     update(frame) {
       super.update(frame);
       let startBEAN = 1464;
-      let startFrame = FRAME_FOR_BEAN(startBEAN);
 
       this.flat.scale.set(0, 0, 0);
-      this.flat.position.set(0, -30, 0);
+      this.flat.position.set(0, 30, 0);
       this.shade.scale.set(0, 0, 0);
       this.shade.position.set(0, 0, 0);
       this.society.scale.set(0, 0, 0);
-      this.society.position.set(0, 30, 0);
+      this.society.position.set(0, -30, 0);
 
-      this.cameraDDX += -this.cameraDX * 1.6 + (Math.random() - 0.5) * smoothstep(0, 1, (frame-startFrame)/100)/4;
-      this.cameraDDY += -this.cameraDY * 1.6 + (Math.random() - 0.5) * smoothstep(0, 1, (frame-startFrame)/100)/4;
-      this.cameraDX = - this.cameraX * 0.5;
-      this.cameraDY = - this.cameraY * 0.5;
-      this.cameraDX *= 0.5;
-      this.cameraDY *= 0.5;
-      this.cameraDX += this.cameraDDX;
-      this.cameraDY += this.cameraDDY;
-      this.cameraX += this.cameraDX;
-      this.cameraY += this.cameraDY;
-      this.cameraX *= 0.5;
-      this.cameraY *= 0.5;
-
-      if (BEAN >= startBEAN + 7) {
-        let s = easeIn(0, 1, F(frame, startBEAN + 7, 3));
-        this.flat.scale.set(s, s, s);
-        this.flat.position.set(this.cameraX, 30 + this.cameraY, 0);
-      }
-      if (BEAN >= startBEAN + 10) {
-        let s = easeIn(0, 1, F(frame, startBEAN + 10, 3));
-        this.shade.scale.set(s, s, s);
-        this.shade.position.set(this.cameraX, this.cameraY, 0);
-      }
-      if (BEAN >= startBEAN + 14) {
-        let s = easeIn(0, 1, F(frame, startBEAN + 14, 3));
-        this.society.scale.set(s, s, s);
-        this.society.position.set(this.cameraX, -30 + this.cameraY, 0);
-      }
+      this.flat.scale.setScalar(easeIn(0, 1, F(frame, startBEAN + 7, 3)));
+      this.shade.scale.setScalar(easeIn(0, 1, F(frame, startBEAN + 10, 3)));
+      this.society.scale.setScalar(easeIn(0, 1, F(frame, startBEAN + 14, 3)));
     }
   }
 
