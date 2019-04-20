c13f81554bbabfd8c024c0c9680af0df60ef4dc3
Stian Jensen <me@stianj.com>
2019-04-20 21:49:03 +0200

    Reset demo after first playback

--- src/splasherNode.js
+++ src/splasherNode.js
@@ -21,13 +21,14 @@
     }
 
     update(frame) {
+      this.inputs.A.enabled = true;
       this.uniforms.generalGrayScaler.value = easeIn(1, 0, F(frame, 96, 24));
       this.uniforms.radiuser.value = 4;
 
       this.uniforms.frame.value = frame;
       this.uniforms.tDiffuse.value = this.inputs.A.getValue();
       this.uniforms.paperContent.value = this.inputs.PaperContent.getValue();
-      this.uniforms.infoOutro.value = this.inputs.B.getValue();
+      this.uniforms.infoOutro.value = BEAN >= 1518 ? this.inputs.B.getValue() : undefined;
 
       this.uniforms.overlayer.value = 0;
       this.uniforms.backgroundiness.value = 1;
