70b1eb3c07f73306b538463930fd2d49bf422b07
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2019-04-20 23:54:05 +0200

    More bugfixes

--- src/splasherNode.js
+++ src/splasherNode.js
@@ -18,10 +18,12 @@
     beforeUpdate() {
       this.inputs.B.enabled = BEAN >= 1518;
       this.inputs.A.enabled = true;
+      this.uniforms.tDiffuse.value = this.inputs.A.getValue();
+      this.uniforms.paperContent.value = this.inputs.PaperContent.getValue();
+      this.uniforms.infoOutro.value = BEAN >= 1518 ? this.inputs.B.getValue() : undefined;
     }
 
     update(frame) {
-      this.inputs.A.enabled = true;
       this.uniforms.generalGrayScaler.value = easeIn(1, 0, F(frame, 96, 24));
       this.uniforms.radiuser.value = 4;
 
@@ -33,6 +35,13 @@
       this.uniforms.overlayer.value = 0;
       this.uniforms.backgroundiness.value = 1;
       this.uniforms.framiness.value = 1;
+    }
+
+    render(renderer) {
+      this.uniforms.tDiffuse.value = this.inputs.A.getValue();
+      this.uniforms.paperContent.value = this.inputs.PaperContent.getValue();
+      this.uniforms.infoOutro.value = BEAN >= 1518 ? this.inputs.B.getValue() : undefined;
+      super.render(renderer);
     }
   }
 
